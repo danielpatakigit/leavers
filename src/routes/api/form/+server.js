83afc355e4ed72c84204e05bdc6a917a1440c81c
Dániel Pataki <e12223375@student.tuwien.ac.at>
2024-03-05 17:19:31 +0100

    req fix

--- src/routes/api/form/+server.js
+++ src/routes/api/form/+server.js
@@ -2,6 +2,7 @@ import { json } from "@sveltejs/kit";
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-	console.log(request.json());
+	const req = await request.json();
+	console.log(req);
 	return { status: 200, body: { message: "Alles gut." } };
 }
