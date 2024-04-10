c13a17c56f82df30c180d171d46f997dd0427d62
Dániel Pataki <e12223375@student.tuwien.ac.at>
2024-04-10 10:52:26 +0200

    Update Header component and +page.server.js

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Link from "./Link.svelte";
 	export let data;
+
+	// when data changes update the header
 </script>
 
 <header class="fixed top-0 z-50 flex w-full justify-center">
@@ -12,37 +14,41 @@
 				<img src="/ms laika.png" alt="ms laika" />
 			</div>
 			<div class="flex flex-col">
-				<span class="-mb-1 font-serif font-bold">
-					{#if data?.user}
-						Leavers' Camp
-					{:else}
-						Milestone
-					{/if}
-				</span>
-				<span class="text-sm">
-					{#if data?.user?.name}
-						<span class="font-light italic">Using as:</span>
-						<br class="md:hidden" />
-						<span class="font-bold text-gold-900">{data?.user?.name}</span>
-					{:else}
-						<span>Leavers' Camp</span>
-					{/if}
-				</span>
+				{#key data?.user}
+					<span class="-mb-1 font-serif font-bold">
+						{#if data?.user}
+							Leavers' Camp
+						{:else}
+							Milestone
+						{/if}
+					</span>
+					<span class="text-sm">
+						{#if data?.user?.name}
+							<span class="font-light italic">Using as:</span>
+							<br class="md:hidden" />
+							<span class="font-bold text-gold-900">{data?.user?.name}</span>
+						{:else}
+							<span>Leavers' Camp</span>
+						{/if}
+					</span>
+				{/key}
 			</div>
 		</a>
 		<div class="flex items-center gap-4">
-			{#if data?.user}
-				<Link
-					href="/myregistration"
-					icon="solar:paperclip-linear"
-					text="My Registration"
-					hot={true}
-				/>
-			{:else}
-				<!-- <Link href="/" icon="" text="Home" hot={false} /> -->
-				<Link href="/login" icon="" text="Login" hot={false} />
-				<Link href="/register" icon="" text="Register" hot={true} />
-			{/if}
+			{#key data?.user}
+				{#if data?.user}
+					<Link
+						href="/myregistration"
+						icon="solar:paperclip-linear"
+						text="My Registration"
+						hot={true}
+					/>
+				{:else}
+					<!-- <Link href="/" icon="" text="Home" hot={false} /> -->
+					<Link href="/login" icon="" text="Login" hot={false} />
+					<Link href="/register" icon="" text="Register" hot={true} />
+				{/if}
+			{/key}
 		</div>
 	</div>
 </header>
