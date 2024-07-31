e1dc2eda326ee2046702d209aca3350ff83ce15f
Dániel Pataki <e12223375@student.tuwien.ac.at>
2024-07-31 11:43:27 +0200

    quick tables

--- src/routes/admin/data/+page.svelte
+++ src/routes/admin/data/+page.svelte
@@ -80,33 +80,68 @@
 	let sortedRecords = data.records.map((record) =>
 		sortKeysByOrder(record, columnOrder),
 	);
+
+	let selectedTab = "tables"; // or tables
 </script>
 
-<h1 class="mt-24 text-center text-3xl font-bold">Data Input Page</h1>
 <!-- <pre>{JSON.stringify(data, null, 2)}</pre> -->
-<table class="min-w-full divide-y divide-gray-200">
-	<thead class="bg-gray-50">
-		{#each sortedRecords as record, i}
-			{#if i === 0}
+<div class="mt-36"></div>
+<div class="*: flex w-full *:flex-1">
+	<button
+		on:click={() => {
+			selectedTab = "general";
+		}}
+	>
+		General
+	</button>
+	<button
+		on:click={() => {
+			selectedTab = "tables";
+		}}
+	>
+		Tables
+	</button>
+</div>
+{#if selectedTab === "general"}
+	<table class="min-w-full divide-y divide-gray-200">
+		<thead class="bg-gray-50">
+			{#each sortedRecords as record, i}
+				{#if i === 0}
+					<tr>
+						{#each Object.keys(record) as key}
+							<th
+								class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+							>
+								{key}
+							</th>
+						{/each}
+					</tr>
+				{/if}
+			{/each}
+		</thead>
+		<tbody class="divide-y divide-gray-200 bg-white">
+			{#each sortedRecords as record, i}
 				<tr>
-					{#each Object.keys(record) as key}
-						<th
-							class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
-						>
-							{key}
-						</th>
+					{#each Object.values(record) as value}
+						<td class="whitespace-nowrap px-6 py-4">{value}</td>
 					{/each}
 				</tr>
-			{/if}
-		{/each}
-	</thead>
-	<tbody class="divide-y divide-gray-200 bg-white">
+			{/each}
+		</tbody>
+	</table>
+{:else if selectedTab === "tables"}
+	<div class="flex flex-col gap-12 border p-2">
 		{#each sortedRecords as record, i}
-			<tr>
-				{#each Object.values(record) as value}
-					<td class="whitespace-nowrap px-6 py-4">{value}</td>
+			<div class="mx-auto flex flex-col gap-1 border border-black p-2 *:flex-1">
+				{#each Object.keys(record) as key}
+					<div class="flex flex-row gap-12 border-b *:flex-1">
+						<div class=" break-words font-bold">{key}</div>
+						<div class=" break-words">{record[key]}</div>
+					</div>
 				{/each}
-			</tr>
+			</div>
 		{/each}
-	</tbody>
-</table>
+	</div>
+{:else}
+	<p>Something went wrong</p>
+{/if}
